12a72d14f4a0dbd539c5c12fa31935c5c5448702
bobbyviki <rong.cai@viki.com>
2022-03-10 18:47:25 +0800

    Added tree and quick sort code

---
+++ algo/sort/quicksort.py
@@ -0,0 +1,53 @@
+def partition(start, end, array):
+    # Initializing pivot's index to start
+    pivot_index = start
+    # Choose the starting element as the index
+    pivot = array[pivot_index]
+     
+    # This loop runs till start pointer crosses
+    # end pointer, and when it does we swap the
+    # pivot with element on end pointer
+    while start < end:
+         
+        # Increment the start pointer till it finds an
+        # element greater than  pivot
+        while start < len(array) and array[start] <= pivot:
+            start += 1
+             
+        # Decrement the end pointer till it finds an
+        # element less than pivot
+        while array[end] > pivot:
+            end -= 1
+         
+        # If start and end have not crossed each other,
+        # swap the numbers on start and end
+        if(start < end):
+            array[start], array[end] = array[end], array[start]
+				
+				# this continues until the start and end pointer cross
+     
+    # Swap pivot element with element on end pointer.
+    # This puts pivot on its correct sorted place.
+		# python swap...
+    array[end], array[pivot_index] = array[pivot_index], array[end]
+    # Returning end pointer to divide the array into 2 about the pivot
+    return end
+     
+# The main function that implements QuickSort
+def quick_sort(start, end, array):
+    if (start < end):
+         
+        # p is partitioning index, array[p]
+        # is at right place
+        p = partition(start, end, array)
+         
+        # Sort elements before partition
+        # and after partition
+        quick_sort(start, p - 1, array)
+        quick_sort(p + 1, end, array)
+         
+# Driver code
+array = [ 10, 7, 8, 9, 1, 5, 29 ]
+quick_sort(0, len(array) - 1, array)
+ 
+print(f'Sorted array: {array}')
